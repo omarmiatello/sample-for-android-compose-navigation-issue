9f7aa06d8ff6c161928151ccdd069021f110dae7
Omar Miatello <jackl85@gmail.com>
2022-05-31 11:59:01 +0200

    first commit

---
+++ app/src/main/java/com/github/omarmiatello/composetest/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.github.omarmiatello.composetest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.delay
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            var bool by remember { mutableStateOf(false) }
+            LaunchedEffect(Unit) {
+                while (true) { // "animation" loop
+                    delay(1)
+                    bool = !bool
+                }
+            }
+            remember { println("I will be always remembered!") }
+            NavHost(
+                navController = rememberNavController(),
+                startDestination = "test1",
+            ) {
+                composable(route = "test1") {
+                    remember { println("I will be remembered, but sometimes forgotten (race condition)") }
+                    DisposableEffect(Unit) {
+                        onDispose { println("disposed (current lifecycle: ${it.lifecycle.currentState}) - triggered by onForgotten()") }
+                    }
+                    Text(text = "Current value: $bool")
+                }
+            }
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("onStop()")
+    }
+}
